b2629c33b07ec3eda2d4490a1ccdbc271d96f5f4
Ian Hamilton <ian.hamilton117@gmail.com>
2018-11-21 15:31:12 -0800

    Adding pagination

--- search.js
+++ search.js
@@ -3,11 +3,21 @@ const apiUrl = "https://www.googleapis.com/customsearch/v1";
 let params = {
     key: "AIzaSyBqcrX7zwelHrLUsH0R8KOI-aMCTlnrp3g", // Google API key
     cx: "014722161919417917553:vmkwzgw4l5u", // Google search engine ID
-    q: ''
+    q: "", // Query
+    start: "" // The index of the first result to return
 }
 
-function basicSearch() {
+//10 is the default number of results per page returned by the Google API. It is also the maximum.
+const resultsPerPage = 10;
+
+let currentPage = 1;
+
+function basicSearch(pageNum) {
+    currentPage = pageNum;
+
     params.q = document.getElementById("searchInput").value;
+    // Calculate which result will be the first result on a given page. 1 for page 1, 11 for page 2, 22 for page 3, etc.
+    params.start = String(resultsPerPage * (pageNum - 1) + 1);
 
     let query = Object.keys(params)
         .map(k => k + '=' + params[k])
@@ -33,9 +43,9 @@ function sendApiRequest(url) {
 }
 
 function populateResultsText(resultsObj) {
-    let resultsString = '';
+    let resultsHtml = '';
     resultsObj.items.forEach(item => {
-        resultsString += `
+        resultsHtml += `
             <hr>
             <div class="result">
                 <a class="title" href="${item.link}">${item.htmlTitle}</a>
@@ -44,6 +54,22 @@ function populateResultsText(resultsObj) {
             </div>
         `;
     });
-    document.getElementById("result-list").innerHTML = resultsString;
+    document.getElementById("result-list").innerHTML = resultsHtml;
+
+    const numResults = resultsObj.queries.request[0].totalResults;
+    //Calculate the number of pages required to fit all results, with a max of 100 results (Google won't return any results past 100)
+    const numPages = Math.floor((Math.min(numResults, 100) - 1) / resultsPerPage) + 1;
+
+    let pageNavHtml = 'Result Page ';
+    for (let i = 1; i <= numPages; i++) {
+        if (i != currentPage) {
+            pageNavHtml += `<a class="page-nav-link" onclick="basicSearch(${i})">${i}</a>`;
+        } else {
+            pageNavHtml += `<span class="page-nav-link">${i}</span>`;
+        }
+    }
+    document.getElementById("page-nav").innerHTML = pageNavHtml;
+
+    // TODO: Include previous/next links
 }
 
