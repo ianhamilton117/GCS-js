ca2d0b7d70ab61620ccd098232a0ec6396061d05
Ian Hamilton <ian.hamilton117@gmail.com>
2018-11-27 12:17:41 -0800

    Downgrading to be compatible with IE 10, which should have been wiped off the face of the earth long ago

--- search.js
+++ search.js
@@ -1,12 +1,12 @@
-const apiUrl = "https://www.googleapis.com/customsearch/v1";
-const key = "AIzaSyBqcrX7zwelHrLUsH0R8KOI-aMCTlnrp3g"; // Google API key
-const cx = "014722161919417917553:vmkwzgw4l5u"; // Google search engine ID
-let searchParams;
+var apiUrl = "https://www.googleapis.com/customsearch/v1";
+var key = "AIzaSyBqcrX7zwelHrLUsH0R8KOI-aMCTlnrp3g"; // Google API key
+var cx = "014722161919417917553:vmkwzgw4l5u"; // Google search engine ID
+var searchParams;
 
-//10 is the default number of results per page returned by the Google API. It is also the maximum.
-const resultsPerPage = 10;
+// 10 is the default number of results per page returned by the Google API. It is also the maximum.
+var resultsPerPage = 10;
 
-let currentPage = 1;
+var currentPage = 1;
 
 function basicSearch() {
     searchParams = {};
@@ -22,6 +22,7 @@ function advancedSearch() {
     searchParams = {};
     searchParams.key = key;
     searchParams.cx = cx;
+    // Passes reqWords in place of the query to get around an apparent bug where Google doesn't return any results if only required words are sent
     searchParams.q = document.getElementById("reqWords").value;
     searchParams.hq = document.getElementById("reqWords").value;
     searchParams.exactTerms = document.getElementById("exactPhrase").value;
@@ -45,23 +46,20 @@ function pageNav(pageNum) {
 }
 
 function executeSearch() {
-    let query = Object.keys(searchParams)
-        .map(k => k + '=' + searchParams[k])
+    var query = Object.keys(searchParams)
+        .map(function(k) {return k + '=' + searchParams[k]})
         .join('&');
 
-    let url = apiUrl + "?" + query;
-    sendApiRequest(url);
+    var url = apiUrl + "?" + query;
 
-}
-
-function sendApiRequest(url) {
-    const http = new XMLHttpRequest();
+    var http = new XMLHttpRequest();
     http.open("GET", url);
     http.send();
-    http.onreadystatechange = e => {
+
+    http.onreadystatechange = function(e) {
         console.log(e);
         try {
-            let resultsObj = JSON.parse(http.response);
+            var resultsObj = JSON.parse(http.response);
             populateResultsText(resultsObj);
         } catch (e) {
             console.error(e);
@@ -70,36 +68,33 @@ function sendApiRequest(url) {
 }
 
 function populateResultsText(resultsObj) {
-    let resultsHtml = '';
-    resultsObj.items.forEach(item => {
-        resultsHtml += `
-            <hr>
-            <div class="result">
-                <a class="title" href="${item.link}">${item.htmlTitle}</a>
-                <p class="snippet">${item.htmlSnippet}</p>
-                <p class="url" title="${item.link}">${item.link}</p>
-            </div>
-        `;
+    var resultsHtml = '';
+    resultsObj.items.forEach(function(item) {
+        resultsHtml += '<hr><div class="result">' +
+            '<a class="title" href="' + item.link + '">' + item.htmlTitle + '</a>' +
+            '<p class="snippet">' + item.htmlSnippet + '</p>' +
+            '<p class="url" title="' + item.link + '">' + item.link + '</p>' +
+            '</div>';
     });
     document.getElementById("result-list").innerHTML = resultsHtml;
 
-    const numResults = resultsObj.queries.request[0].totalResults;
-    //Calculate the number of pages required to fit all results, with a max of 100 results (Google won't return any results past 100)
-    const numPages = Math.floor((Math.min(numResults, 100) - 1) / resultsPerPage) + 1;
+    var numResults = resultsObj.queries.request[0].totalResults;
+    // Calculate the number of pages required to fit all results, with a max of 100 results (Google won't return any results past 100)
+    var numPages = Math.floor((Math.min(numResults, 100) - 1) / resultsPerPage) + 1;
 
-    let pageNavHtml = 'Result Page ';
+    var pageNavHtml = 'Result Page ';
     if (currentPage > 1) {
-        pageNavHtml += `<a class="page-nav-link" onclick="pageNav(${currentPage - 1})">Previous</a>`
+        pageNavHtml += '<a class="page-nav-link" onclick="pageNav(' + (currentPage - 1) + ')">Previous</a>';
     }
-    for (let i = 1; i <= numPages; i++) {
+    for (var i = 1; i <= numPages; i++) {
         if (i != currentPage) {
-            pageNavHtml += `<a class="page-nav-link" onclick="pageNav(${i})">${i}</a>`;
+            pageNavHtml += '<a class="page-nav-link" onclick="pageNav(' + i + ')">' + i + '</a>';
         } else {
-            pageNavHtml += `<span class="page-nav-link">${i}</span>`;
+            pageNavHtml += '<span class="page-nav-link">' + i + '</span>';
         }
     }
     if (currentPage < numPages) {
-        pageNavHtml += `<a class="page-nav-link" onclick="pageNav(${currentPage + 1})">Next</a>`
+        pageNavHtml += '<a class="page-nav-link" onclick="pageNav(' + (currentPage + 1) + ')">Next</a>';
     }
     document.getElementById("page-nav").innerHTML = pageNavHtml;
 }
