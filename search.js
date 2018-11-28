0b4c73648ac9ab6d1e2e1597918f9a2bcb28ff3c
Ian Hamilton <ian.hamilton117@gmail.com>
2018-11-27 16:45:29 -0800

    Changing names of global variables to reduce probability of shared names

--- search.js
+++ search.js
@@ -1,43 +1,43 @@
-var apiUrl = "https://www.googleapis.com/customsearch/v1";
-var key = "AIzaSyBbKSgHIjTElfsgiZFU-U_KwVld-0sgdes"; // Google API key
-var cx = "014722161919417917553:vmkwzgw4l5u"; // Google search engine ID
-var searchParams;
+var gcs_apiUrl = "https://www.googleapis.com/customsearch/v1";
+var gcs_key = "AIzaSyBbKSgHIjTElfsgiZFU-U_KwVld-0sgdes"; // Google API key
+var gcs_cx = "014722161919417917553:vmkwzgw4l5u"; // Google search engine ID
+var gcs_searchParams;
 
 // 10 is the default number of results per page returned by the Google API. It is also the maximum.
-var resultsPerPage = 10;
+var gcs_resultsPerPage = 10;
 
-var currentPage = 1;
+var gcs_currentPage = 1;
 
 function basicSearch() {
-    searchParams = {};
-    searchParams.key = key;
-    searchParams.cx = cx;
-    searchParams.q = document.getElementById("searchInput").value;
-    searchParams.start = 1;
+    gcs_searchParams = {};
+    gcs_searchParams.key = gcs_key;
+    gcs_searchParams.cx = gcs_cx;
+    gcs_searchParams.q = document.getElementById("searchInput").value;
+    gcs_searchParams.start = 1;
 
     executeSearch();
 }
 
 function advancedSearch() {
-    searchParams = {};
-    searchParams.key = key;
-    searchParams.cx = cx;
+    gcs_searchParams = {};
+    gcs_searchParams.key = gcs_key;
+    gcs_searchParams.cx = gcs_cx;
     // Passes reqWords in place of the query to get around an apparent bug where Google doesn't return any results if only required words are sent
-    searchParams.q = document.getElementById("reqWords").value;
-    searchParams.hq = document.getElementById("reqWords").value;
-    searchParams.exactTerms = document.getElementById("exactPhrase").value;
-    searchParams.orTerms = document.getElementById("anyWords").value;
-    searchParams.excludeTerms = document.getElementById("withoutWords").value;
-    searchParams.fileType = document.getElementById("fileFormat").value;
-    searchParams.start = 1;
+    gcs_searchParams.q = document.getElementById("reqWords").value;
+    gcs_searchParams.hq = document.getElementById("reqWords").value;
+    gcs_searchParams.exactTerms = document.getElementById("exactPhrase").value;
+    gcs_searchParams.orTerms = document.getElementById("anyWords").value;
+    gcs_searchParams.excludeTerms = document.getElementById("withoutWords").value;
+    gcs_searchParams.fileType = document.getElementById("fileFormat").value;
+    gcs_searchParams.start = 1;
 
     executeSearch();
 }
 
 function pageNav(pageNum) {
-    currentPage = pageNum;
+    gcs_currentPage = pageNum;
     // Calculate which result will be the first result on a given page. 1 for page 1, 11 for page 2, 22 for page 3, etc.
-    searchParams.start = String(resultsPerPage * (pageNum - 1) + 1);
+    gcs_searchParams.start = String(gcs_resultsPerPage * (pageNum - 1) + 1);
 
     executeSearch();
 
@@ -46,11 +46,11 @@ function pageNav(pageNum) {
 }
 
 function executeSearch() {
-    var query = Object.keys(searchParams)
-        .map(function(k) {return k + '=' + searchParams[k]})
+    var query = Object.keys(gcs_searchParams)
+        .map(function(k) {return k + '=' + gcs_searchParams[k]})
         .join('&');
 
-    var url = apiUrl + "?" + query;
+    var url = gcs_apiUrl + "?" + query;
 
     var http = new XMLHttpRequest();
     http.open("GET", url);
@@ -62,13 +62,13 @@ function executeSearch() {
                 var resultsObj = JSON.parse(http.response);
                 var numResults = resultsObj.queries.request[0].totalResults;
                 if (numResults == 0) {
-                    if (currentPage <= 1) {
+                    if (gcs_currentPage <= 1) {
                         document.getElementById("result-list").innerHTML = 'Your search did not match any results.';
                         document.getElementById("page-nav").innerHTML = '';
                     } else {
                         // If the page requested doesn't have any results, we try again with the previous page. 
                         // This is neccessary because Google's API sometimes promises more results than it actually has.
-                        pageNav(currentPage - 1);
+                        pageNav(gcs_currentPage - 1);
                     }
                 } else {
                     populateResultsText(resultsObj);
@@ -93,21 +93,21 @@ function populateResultsText(resultsObj) {
 
     var numResults = resultsObj.queries.request[0].totalResults;
     // Calculate the number of pages required to fit all results, with a max of 100 results (Google won't return any results past 100)
-    var numPages = Math.floor((Math.min(numResults, 100) - 1) / resultsPerPage) + 1;
+    var numPages = Math.floor((Math.min(numResults, 100) - 1) / gcs_resultsPerPage) + 1;
 
     var pageNavHtml = 'Result Page ';
-    if (currentPage > 1) {
-        pageNavHtml += '<a class="page-nav-link" onclick="pageNav(' + (currentPage - 1) + ')">Previous</a>';
+    if (gcs_currentPage > 1) {
+        pageNavHtml += '<a class="page-nav-link" onclick="pageNav(' + (gcs_currentPage - 1) + ')">Previous</a>';
     }
     for (var i = 1; i <= numPages; i++) {
-        if (i != currentPage) {
+        if (i != gcs_currentPage) {
             pageNavHtml += '<a class="page-nav-link" onclick="pageNav(' + i + ')">' + i + '</a>';
         } else {
             pageNavHtml += '<span class="page-nav-link">' + i + '</span>';
         }
     }
-    if (currentPage < numPages) {
-        pageNavHtml += '<a class="page-nav-link" onclick="pageNav(' + (currentPage + 1) + ')">Next</a>';
+    if (gcs_currentPage < numPages) {
+        pageNavHtml += '<a class="page-nav-link" onclick="pageNav(' + (gcs_currentPage + 1) + ')">Next</a>';
     }
     document.getElementById("page-nav").innerHTML = pageNavHtml;
 }
